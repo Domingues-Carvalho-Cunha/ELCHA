8487eb4fc7db83b93780220bc5a74762031c4b16
Eduardo Domingues <20200377@iade.pt>
2021-06-27 18:17:32 +0100

    Controlerr e repository
    Melhoramento adiciona e apaga

--- src/main/java/pt/iade/elchadb/models/repositories/UserRepository.java
+++ src/main/java/pt/iade/elchadb/models/repositories/UserRepository.java
@@ -1,14 +1,15 @@
 package pt.iade.elchadb.models.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.elchadb.models.AppUser;
 
 
 public interface UserRepository extends CrudRepository<AppUser,Integer> {
-
+/*
     // QUERIES
-    String QueryFindUserInfo =
+    @QueryFindUserInfo =
         "SELECT Us_id AS id, "+
         "Us_firstName AS nome, "+
         "Us_lastName AS apelido, "+
@@ -24,14 +25,23 @@ public interface UserRepository extends CrudRepository<AppUser,Integer> {
         "INNER JOIN planoestudos p ON u.dis_id=p.pla_dis_id "+
         "INNER JOIN cursos c ON p.pla_cur_id=c.cur_id";
         
+*/
+
+    @Query(value=
+        "SELECT ut_id AS id, ut_nome AS Nome, "+
+        "ut_surname AS Apelido, "+
+        "ut_genero AS Genero, "+
+        "ut_danasc AS Data Nascimento",
+    nativeQuery=true)
+    AppUser FindUserId( int UserId);
+
+    
     // COMANDS
     AppUser save(User user);
-    AppUser FindUserId( int UserId);
-    Iterable<AppUser> findByNameContaining(String text);
-    
-    Iterable<AppUser> findByName(String name);
-    Iterable<AppUser> findByPointsBetween(int min,int max);
-    Iterable<AppUser> findByNameContainingAndPointsBetween (String name,int min,int max);
+    //Iterable<AppUser> findByName(String name);
+    //Iterable<AppUser> findByNameContaining(String text);
+    //Iterable<AppUser> findByPointsBetween(int min,int max);
+    //Iterable<AppUser> findByNameContainingAndPointsBetween (String name,int min,int max);
 }
 
 
