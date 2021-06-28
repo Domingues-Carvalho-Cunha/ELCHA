655cb00214c01742132a7031cf1828c28c41f80a
Eduardo Domingues <80538350+OdraudePT@users.noreply.github.com>
2021-06-28 01:24:08 +0100

    Get por nome erro
    continua com erro, não consigo arranjar

--- src/main/java/pt/iade/elchadb/controllers/UsersController.java
+++ src/main/java/pt/iade/elchadb/controllers/UsersController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import javassist.NotFoundException;
 import pt.iade.elchadb.models.AppUser;
@@ -45,7 +45,7 @@ public class UsersController {
   }
 
   // SALVA UM ULTILIZADOR NOVO
-  @PostMapping(path = "/saveUser", produces= MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(path = "/saveUser/{Us_firstName}/{Us_lastName}/{Us_gender}/{Us_email}/{Us_dob}/{Us_points}/{Us_level}/{Us_gems}/{Us_status}", produces= MediaType.APPLICATION_JSON_VALUE)
   public AppUser saveUser(@RequestBody AppUser User) {
     AppUser savedUser = UserRepository.save(User);
     logger.info("Saving User with id "+((AppUser) savedUser).getId());
@@ -66,6 +66,14 @@ public class UsersController {
 
     }   
   }
+
+/*
+  @GetMapping(path ="/filtro/{Us_firstName}", produces= MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<AppUser> getUserNome(@PathVariable String Us_firstName) {
+      logger.info("A carregar a Leadearboard");
+      return UserRepository.findByNameContaining();
+  }
+
 
   // DEVOLVE UM UTILIZADOR PELA PESQUISA DO NOME
   @GetMapping(path = "/{text:[^0-9]+}", produces= MediaType.APPLICATION_JSON_VALUE)
@@ -89,5 +97,6 @@ public class UsersController {
       try { _pointsMax = Integer.parseInt(pointsMax);
       } catch (NumberFormatException e) {}
     return UserRepository.findByNameContainingAndPointsBetween(name, _pointsMin, _pointsMax);
-  } 
+  }
+*/
 }
--- src/main/java/pt/iade/elchadb/models/repositories/UserRepository.java
+++ src/main/java/pt/iade/elchadb/models/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package pt.iade.elchadb.models.repositories;
 
 import org.springframework.data.jpa.repository.Query;
+
+//import javax.sql.rowset.spi.TransactionalWriter;
+
 import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 import pt.iade.elchadb.models.AppUser;
@@ -21,23 +24,29 @@ public interface UserRepository extends CrudRepository<AppUser,Integer> {
 
     // QUERIES
     @Query(value=
-        "SELECT ut_id AS id, ut_nome AS Nome, "+
+        "SELECT ut_id AS id, "+
+        "ut_nome AS Nome, "+
         "ut_surname AS Apelido, "+
         "ut_genero AS Genero, "+
         "ut_danasc AS Data Nascimento",
     nativeQuery=true)
-    AppUser FindUserId( int UserId);
+    AppUser FindUserId( int UserId);   
+    
+    AppUser save(User user);
 
-
-
+/*
     String findName = ("select Us_firstName from AppUser where Us_firstName =: firstName");
-    Iterable<AppUser> findByNameContaining(String text);
+
+    @Modifying @Transactional
+    @Query(value=findName,nativeQuery = true)
+    Iterable<AppUser> findByNameContaining();
 
 
     String findSomeone = ("select * from AppUser where Us_firstName, Us_points =: firstName, points");
     Iterable<AppUser> findByName(String name);
     Iterable<AppUser> findByPointsBetween(int min,int max);
     Iterable<AppUser> findByNameContainingAndPointsBetween(String name, int _pointsMin, int _pointsMax);
+*/
 }
 
 
